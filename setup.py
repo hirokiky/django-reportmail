666eb39b65c9dbd163442637a8186cc645fb9e20
Hiroki KIYOHARA <hirokiky@gmail.com>
2014-05-24 18:31:59 +0900

    Improved setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,30 @@
+import os
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
 
 setup(
     name='django-reportmail',
-    version='0.1',
+    version='1.0b1',
     packages=find_packages('reportmail'),
     url='https://github.com/hirokiky/django-reportmail',
     license='MIT',
     author='hirokiky',
     author_email='hirokiky@gmail.com',
     description='django library to render and send report mail. ',
+    loag_description=README,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Web Environment",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Framework :: Django",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
     install_requires=[
         'Django>=1.6,<1.7',
     ],
